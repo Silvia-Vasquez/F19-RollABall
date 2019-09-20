8013d6742f0d99688316a8d7b49966cc3818e3da
Silvia Vasquez <falcon19@scarletmail.rutgers.edu>
2019-09-20 12:24:30 -0400

    camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject sphere;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - sphere.transform.position;
+    }
+    private void LateUpdate()
+    {
+        transform.position = sphere.transform.position + offset;
+    }
+}
